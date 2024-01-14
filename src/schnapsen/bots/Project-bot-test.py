9de0582a7db1c352a457653e0afd3f8bd36b154b
T-semeleer <t.e.semeleer@student.vu.nl>
2024-01-14 13:54:06 +0100

    'Just a test' 'origin/main'

---
+++ src/schnapsen/bots/Project-bot-test.py
@@ -0,0 +1,12 @@
+from schnapsen.game import Bot, PlayerPerspective, SchnapsenDeckGenerator, Move, Trick, GamePhase
+from typing import Optional, cast, Literal
+from schnapsen.deck import Suit, Rank
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
+import joblib
+import time
+import pathlib
+
+'''
+This is just a test file that we can start creating the bot and for me to make sure it syncs in the github
+'''
