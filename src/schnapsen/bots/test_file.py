c6bb7773ed8d1a7a803cbc96c72537034608f320
rohanmenon04 <rohanshreyasmenon@gmail.com>
2024-01-15 22:16:21 +0800

    'Set up a test file so we can more easily test specific functions' 'origin/main'

---
+++ src/schnapsen/bots/test_file.py
@@ -0,0 +1,103 @@
+from schnapsen.game import Card, RegularMove, Suit, Rank, Move, Marriage
+from phase1_ml_bot import Agent
+import numpy as np
+from typing import Optional
+
+def get_one_hot_encoding_of_card_suit(card_suit: Suit) -> list[int]:
+    """
+    Translating the suit of a card into one hot vector encoding of size 4.
+    """
+    card_suit_one_hot: list[int]
+    if card_suit == Suit.HEARTS:
+        card_suit_one_hot = [0, 0, 0, 1]
+    elif card_suit == Suit.CLUBS:
+        card_suit_one_hot = [0, 0, 1, 0]
+    elif card_suit == Suit.SPADES:
+        card_suit_one_hot = [0, 1, 0, 0]
+    elif card_suit == Suit.DIAMONDS:
+        card_suit_one_hot = [1, 0, 0, 0]
+    else:
+        raise ValueError("Suit of card was not found!")
+
+    return card_suit_one_hot
+
+
+def get_one_hot_encoding_of_card_rank(card_rank: Rank) -> list[int]:
+    """
+    Translating the rank of a card into one hot vector encoding of size 13.
+    """
+    card_rank_one_hot: list[int]
+    if card_rank == Rank.ACE:
+        card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    elif card_rank == Rank.TWO:
+        card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+    elif card_rank == Rank.THREE:
+        card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+    elif card_rank == Rank.FOUR:
+        card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    elif card_rank == Rank.FIVE:
+        card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+    elif card_rank == Rank.SIX:
+        card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    elif card_rank == Rank.SEVEN:
+        card_rank_one_hot = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+    elif card_rank == Rank.EIGHT:
+        card_rank_one_hot = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    elif card_rank == Rank.NINE:
+        card_rank_one_hot = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+    elif card_rank == Rank.TEN:
+        card_rank_one_hot = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    elif card_rank == Rank.JACK:
+        card_rank_one_hot = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    elif card_rank == Rank.QUEEN:
+        card_rank_one_hot = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    elif card_rank == Rank.KING:
+        card_rank_one_hot = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    else:
+        raise AssertionError("Provided card Rank does not exist!")
+    return card_rank_one_hot
+
+
+def get_move_feature_vector(move: Optional[Move]) -> list[int]:
+    """
+        In case there isn't any move provided move to encode, we still need to create a "padding"-"meaningless" vector of the same size,
+        filled with 0s, since the ML models need to receive input of the same dimensionality always.
+        Otherwise, we create all the information of the move i) move type, ii) played card rank and iii) played card suit
+        translate this information into one-hot vectors respectively, and concatenate these vectors into one move feature representation vector
+    """
+
+    if move is None:
+        move_type_one_hot_encoding_numpy_array = [0, 0, 0]
+        card_rank_one_hot_encoding_numpy_array = [0, 0, 0, 0]
+        card_suit_one_hot_encoding_numpy_array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    else:
+        move_type_one_hot_encoding: list[int]
+        # in case the move is a marriage move
+        if move.is_marriage():
+            move_type_one_hot_encoding = [0, 0, 1]
+            card = move.queen_card
+        #  in case the move is a trump exchange move
+        elif move.is_trump_exchange():
+            move_type_one_hot_encoding = [0, 1, 0]
+            card = move.jack
+        #  in case it is a regular move
+        else:
+            move_type_one_hot_encoding = [1, 0, 0]
+            card = move.card
+        move_type_one_hot_encoding_numpy_array = move_type_one_hot_encoding
+        card_rank_one_hot_encoding_numpy_array = get_one_hot_encoding_of_card_rank(card.rank)
+        card_suit_one_hot_encoding_numpy_array = get_one_hot_encoding_of_card_suit(card.suit)
+
+    return np.array(move_type_one_hot_encoding_numpy_array + card_rank_one_hot_encoding_numpy_array + card_suit_one_hot_encoding_numpy_array)
+
+move1 = RegularMove(Card.KING_CLUBS)
+move2 = RegularMove(Card.ACE_SPADES)
+agent = Agent(5)
+sample_encoding = get_move_feature_vector(move1)
+test_encoding = agent.get_feature_vector(move2)
+
+print (sample_encoding)
+print (test_encoding)
+
+print (get_move_feature_vector(move1) == agent.get_feature_vector(move2))
