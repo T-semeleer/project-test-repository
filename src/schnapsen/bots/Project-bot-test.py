cc7d6f3a57d0c97babaf0eff791435f4eb7e28ca
rohanmenon04 <rohanshreyasmenon@gmail.com>
2024-01-15 14:00:35 +0800

    'Imported Card class' 'origin/main'

--- src/schnapsen/bots/Project-bot-test.py
+++ src/schnapsen/bots/Project-bot-test.py
@@ -1,12 +1,12 @@
 from schnapsen.game import Bot, PlayerPerspective, SchnapsenDeckGenerator, Move, Trick, GamePhase
 from typing import Optional, cast, Literal
-from schnapsen.deck import Suit, Rank
+from schnapsen.deck import Suit, Rank, Card
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
 import joblib
 import time
 import pathlib
-import tensorflow as tf
+#import tensorflow as tf
 
 
 # git config pull.rebase false
