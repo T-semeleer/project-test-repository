3bbb60e5032e780d4999267e06cc4f2b35e08e35
rohanmenon04 <rohanshreyasmenon@gmail.com>
2024-01-15 19:23:59 +0800

    'Testing some implementations with TensorFlow' 'origin/main'

--- src/schnapsen/bots/phase1_ml_bot.py
+++ src/schnapsen/bots/phase1_ml_bot.py
@@ -26,7 +26,7 @@ class DQN(tf.keras.Model):
         x = self.dense2(x)
         return self.output_layer(x)
 
-class Agent(PlayerPerspective):
+class Agent:
     """Reinforcement Learning agent that uses the DQN algorithm to select actions."""
     def __init__(self, action_size: int):
         self.model = DQN(action_size)
@@ -36,34 +36,13 @@ class Agent(PlayerPerspective):
         self.memory = [] # Replay memory
         self.gamma = 0.95 # Discount rate for future rewards
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    
-    def get_game_history(self) -> list[tuple[PlayerPerspective, Trick | None]]:
-        return super().get_game_history()
-    def get_trump_suit(self) -> Suit:
-        return super().get_trump_suit()
-    def get_trump_card(self) -> Card | None:
-        return super().get_trump_card()
-    def get_talon_size(self) -> int:
-        return super().get_talon_size()
-    def get_phase(self) -> GamePhase:
-        return super().get_phase()
-    def __get_opponent_bot_state(self) -> BotState:
-        return super().__get_opponent_bot_state()
-    def __get_own_bot_state(self) -> BotState:
-        return super().__get_own_bot_state()
-    def seen_cards(self, leader_move: Move | None) -> CardCollection:
-        return super().seen_cards(leader_move)
-    def __past_tricks_cards(self) -> set[Card]:
-        return super().__past_tricks_cards()
-    def get_known_cards_of_opponent_hand(self) -> CardCollection:
-        return super().get_known_cards_of_opponent_hand()
-    def get_engine(self) -> GamePlayEngine:
-        return super().get_engine()
-    def get_state_in_phase_two(self) -> GameState:
-        return super().get_state_in_phase_two()
-    def make_assumption(self, leader_move: Move | None, rand: Random) -> GameState:
-        return super().make_assumption(leader_move, rand)
-    
+    def encode_card(self, card: Card) -> int:
+        """Convert cards into a numerical value"""
+        if card is None:
+            return 0
+        suit_value = card.suit.value * 10
+        rank_value = card.rank.value
+        return suit_value + rank_value
     def memorize(self, state: np.ndarray, action: int, reward: float, next_state: np.ndarray, done: bool) -> None:
         """Store past experiences in the replay memory."""
         self.memory.append((state, action, reward, next_state, done))
@@ -89,5 +68,3 @@ class Agent(PlayerPerspective):
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay  # Decrease epsilon
-
-Agent(PlayerPerspective())
