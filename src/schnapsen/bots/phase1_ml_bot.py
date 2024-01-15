fad606fad9848f2c492541ef01363df22db7c733
rohanmenon04 <rohanshreyasmenon@gmail.com>
2024-01-15 22:18:23 +0800

    '#' 'origin/main'

--- src/schnapsen/bots/phase1_ml_bot.py
+++ src/schnapsen/bots/phase1_ml_bot.py
@@ -1,5 +1,5 @@
 from random import Random
-from schnapsen.game import Bot, BotState, GamePlayEngine, GameState, Hand, PlayerPerspective, SchnapsenDeckGenerator, Move, Score, Trick, GamePhase
+from schnapsen.game import Bot, BotState, GamePlayEngine, GameState, Hand, PlayerPerspective, SchnapsenDeckGenerator, Move, Score, Trick, GamePhase, RegularMove
 from typing import Optional, cast, Literal
 from schnapsen.deck import CardCollection, Suit, Rank, Card
 from sklearn.neural_network import MLPClassifier
@@ -36,13 +36,70 @@ class Agent:
         self.memory = [] # Replay memory
         self.gamma = 0.95 # Discount rate for future rewards
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    def encode_card(self, card: Card) -> int:
-        """Convert cards into a numerical value"""
-        if card is None:
-            return 0
-        suit_value = card.suit.value * 10
-        rank_value = card.rank.value
-        return suit_value + rank_value
+    def encode_suits(self, card_suit: Card) -> list[int]:
+        card_suit_one_hot: list[int]
+        if card_suit == Suit.HEARTS:
+            card_suit_one_hot = [0, 0, 0, 1]
+        elif card_suit == Suit.CLUBS:
+            card_suit_one_hot = [0, 0, 1, 0]
+        elif card_suit == Suit.SPADES:
+            card_suit_one_hot = [0, 1, 0, 0]
+        elif card_suit == Suit.DIAMONDS:
+            card_suit_one_hot = [1, 0, 0, 0]
+        else:
+            raise ValueError("Suit of card was not found!")
+        return card_suit_one_hot
+    def encode_ranks(self, card_rank):
+        card_rank_one_hot: list[int]
+        if card_rank == Rank.ACE:
+            card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        elif card_rank == Rank.TWO:
+            card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+        elif card_rank == Rank.THREE:
+            card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+        elif card_rank == Rank.FOUR:
+            card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        elif card_rank == Rank.FIVE:
+            card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+        elif card_rank == Rank.SIX:
+            card_rank_one_hot = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+        elif card_rank == Rank.SEVEN:
+            card_rank_one_hot = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+        elif card_rank == Rank.EIGHT:
+            card_rank_one_hot = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+        elif card_rank == Rank.NINE:
+            card_rank_one_hot = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+        elif card_rank == Rank.TEN:
+            card_rank_one_hot = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        elif card_rank == Rank.JACK:
+            card_rank_one_hot = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        elif card_rank == Rank.QUEEN:
+            card_rank_one_hot = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        elif card_rank == Rank.KING:
+            card_rank_one_hot = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        else:
+            raise AssertionError("Provided card Rank does not exist!")
+        return card_rank_one_hot
+    def get_feature_vector(self, move: Move) -> list[int]:
+        if move is None:
+            move_type_arr_numpy = [0, 0, 0]
+            rank_encoding_arr_numpy = [0, 0, 0, 0]
+            suit_encoding_arr_numpy = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        else:
+            if move.is_marriage():
+                move_type_arr = [0, 0, 1]
+                card = move.queen_card
+            elif move.is_trump_exchange():
+                move_type_arr = [0, 1, 0]
+                card = move.jack
+            else:
+                move_type_arr = [1, 0, 0]
+                card = move.cards[0]
+        move_type_arr_numpy = move_type_arr
+        rank_encoding_arr_numpy = self.encode_ranks(card.rank)
+        suit_encoding_arr_numpy = self.encode_suits(card.suit)
+
+        return np.array(move_type_arr_numpy + rank_encoding_arr_numpy + suit_encoding_arr_numpy)
     def memorize(self, state: np.ndarray, action: int, reward: float, next_state: np.ndarray, done: bool) -> None:
         """Store past experiences in the replay memory."""
         self.memory.append((state, action, reward, next_state, done))
@@ -68,3 +125,7 @@ class Agent:
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay  # Decrease epsilon
+
+agent = Agent(5)
+move = RegularMove(Card.KING_DIAMONDS)
+print (agent.get_feature_vector(move))
